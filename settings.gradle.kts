7a8cb81cdd46b23d0c34d5edaac2f1841e45fcb5
송준섭 <wnstjq7979@gmail.com>
2024-04-09 15:04:22 +0900

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "soongan-backend"
---
+++ src/main/kotlin/com/soongan/soonganbackend/SoonganBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.soongan.soonganbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SoonganBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<SoonganBackendApplication>(*args)
+}
