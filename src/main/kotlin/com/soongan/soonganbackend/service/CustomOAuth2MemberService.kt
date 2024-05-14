9c0c369a07f495e4f6cf71d1b506e5c57618f6a9
송준섭 <wnstjq7979@gmail.com>
2024-05-14 10:37:39 +0900

    [#3] remove: ProviderMember 제거, 관련 코드 수정

--- src/main/kotlin/com/soongan/soonganbackend/service/CustomOAuth2MemberService.kt
+++ src/main/kotlin/com/soongan/soonganbackend/service/CustomOAuth2MemberService.kt
@@ -1,6 +1,6 @@
 package com.soongan.soonganbackend.service
 
-import com.soongan.soonganbackend.model.ProviderMember
+import com.soongan.soonganbackend.model.MemberEntity
 import com.soongan.soonganbackend.repository.MemberRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
@@ -17,15 +17,18 @@ class CustomOAuth2MemberService @Autowired constructor(
     override fun loadUser(userRequest: OAuth2UserRequest): OAuth2User {  // OAuth2 로그인을 하면 이 메서드가 호출됨
         val clientRegistration = userRequest.clientRegistration
         val oAuth2User = DefaultOAuth2UserService().loadUser(userRequest)  // OAuth2 로그인을 완료한 유저 정보를 담은 객체
-
-        val providerMember = ProviderMember(  // 직접 구현한 ProviderMember 객체를 생성
-            oAuth2User = oAuth2User,
-            clientRegistration = clientRegistration
-        )
+        val attributes = oAuth2User.attributes  // OAuth2User의 정보를 담은 Map 객체
 
         // 만약 해당 이메일로 가입된 회원이 없다면(처음 로그인하는 유저라면) 회원 정보를 저장
-        memberRepository.findByEmail(providerMember.email)
-            ?: memberRepository.save(providerMember.toMemberEntity())
+        memberRepository.findByEmail(attributes["email"].toString())
+            ?: memberRepository.save(MemberEntity(
+                email = attributes["email"].toString(),
+                nickname = null,
+                birthDate = null,
+                profileImageUrl = null,
+                provider = clientRegistration.registrationId,
+                authorities = oAuth2User.authorities.map { it.authority }
+            ))
 
         return oAuth2User
     }
