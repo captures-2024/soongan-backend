48948d9f1e419a97a369e7936fdfc160d2712de2
송준섭 <wnstjq7979@gmail.com>
2024-05-14 10:33:03 +0900

    [#3] feat: MemberEntity에@EnableJpaAuditing을 활용한 createdAt, updatedAt 변수 추가

--- src/main/kotlin/com/soongan/soonganbackend/model/MemberEntity.kt
+++ src/main/kotlin/com/soongan/soonganbackend/model/MemberEntity.kt
@@ -5,13 +5,18 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 @Entity
+@Table(name = "member")
 @EnableJpaAuditing
 data class MemberEntity(
-    @Column
+    @Column(unique = true, nullable = false)
     val email: String,
 
     @Column
@@ -32,4 +37,14 @@ data class MemberEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Long? = null
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now()
+        protected set
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    var updatedAt: LocalDateTime = LocalDateTime.now()
+        protected set
 }
