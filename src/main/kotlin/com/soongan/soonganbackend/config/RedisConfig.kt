1338a8973db52c2d673f17c4fd3ba385ff3335f3
송준섭 <wnstjq7979@gmail.com>
2024-05-16 13:25:29 +0900

    [#3] config: RedisConfig 추가

---
+++ src/main/kotlin/com/soongan/soonganbackend/config/RedisConfig.kt
@@ -0,0 +1,23 @@
+package com.soongan.soonganbackend.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+
+
+@Configuration
+class RedisConfig {
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        return LettuceConnectionFactory("localhost", 6379)  // 추후 비밀 정보로 환경변수 설정 필요
+    }
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, Any> {
+        val redisTemplate = RedisTemplate<String, Any>()  // 이 redisTemplate으로 Redis에 접근하여 상호작용(삽입, 수정, 삭제,,) 가능
+        redisTemplate.connectionFactory = redisConnectionFactory()
+        return redisTemplate
+    }
+}
