207a39e981ce5a25d8bb44e6ae2185203e522a06
송준섭 <wnstjq7979@gmail.com>
2024-05-29 21:13:41 +0900

    [#12] feat: RedisConfig에 EnableCaching 어노테이션 추가

--- src/main/kotlin/com/soongan/soonganbackend/config/RedisConfig.kt
+++ src/main/kotlin/com/soongan/soonganbackend/config/RedisConfig.kt
@@ -1,5 +1,6 @@
 package com.soongan.soonganbackend.config
 
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
@@ -8,6 +9,7 @@ import org.springframework.data.redis.core.RedisTemplate
 
 
 @Configuration
+@EnableCaching
 class RedisConfig {
     @Bean
     fun redisConnectionFactory(): RedisConnectionFactory {
