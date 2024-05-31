220da480637f3beac0421102c3fbaa933d051008
송준섭 <wnstjq7979@gmail.com>
2024-05-31 21:03:26 +0900

    refactor:  oauth2login 관련 설정 제거

--- src/main/kotlin/com/soongan/soonganbackend/config/SecurityConfig.kt
+++ src/main/kotlin/com/soongan/soonganbackend/config/SecurityConfig.kt
@@ -2,9 +2,6 @@ package com.soongan.soonganbackend.config
 
 import com.soongan.soonganbackend.filter.JwtExceptionFilter
 import com.soongan.soonganbackend.filter.JwtFilter
-import com.soongan.soonganbackend.handler.OAuth2LoginFailureHandler
-import com.soongan.soonganbackend.handler.OAuth2LoginSuccessHandler
-import com.soongan.soonganbackend.persistence.member.CustomOAuth2MemberService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -17,9 +14,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 class SecurityConfig(
-    private val customOAuth2MemberService: CustomOAuth2MemberService,
-    private val oAuth2LoginSuccessHandler: OAuth2LoginSuccessHandler,
-    private val oAuth2LoginFailureHandler: OAuth2LoginFailureHandler,
     private val jwtFilter: JwtFilter,
     private val jwtExceptionFilter: JwtExceptionFilter
 ) {
@@ -38,22 +32,9 @@ class SecurityConfig(
                 // STATELESS: 세션을 생성하지도 않고, 있어도 사용하지 않음
                 it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             }
-            .httpBasic {  // OAuth2 인증을 사용, 필요없는 기본 인증을 비활성화
-                it.disable()
-            }
-            .formLogin {  // OAuth2 인증을 사용, 필요없는 폼 로그인을 비활성화
-                it.disable()
-            }
             .authorizeHttpRequests {  // 요청에 대한 인증 요구 여부 설정
-                it.anyRequest().authenticated()  // 일단 모든 요청에 대해 인증 요구
-            }
-            .oauth2Login {  // OAuth2 인증 설정
-                // 기본 DefaultOAuth2UserService 대신 직접 구현한 CustomOAuth2MemberService 사용
-                it.userInfoEndpoint { endpoint ->
-                    endpoint.userService(customOAuth2MemberService)
-                }
-                    .successHandler(oAuth2LoginSuccessHandler)  // OAuth2 인증 성공 시 처리할 핸들러 설정
-                    .failureHandler(oAuth2LoginFailureHandler)  // OAuth2 인증 실패 시 처리할 핸들러 설정
+                it.requestMatchers("/members/login", "/members/refresh").permitAll()  // /members/login 요청은 인증 없이 허용
+                    .anyRequest().authenticated()  // 일단 모든 요청에 대해 인증 요구
             }
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java) // JwtFilter를 UsernamePasswordAuthenticationFilter 앞에 추가
             // UsernamePasswordAuthenticationFilter는 UsernamePasswordAuthenticationToken을 생성하는 역할 -> JwtFilter에서 대신 Token 생성
