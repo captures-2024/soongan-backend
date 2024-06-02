75e0f21cd23b5237671d4a92c3a053835b23edec
송준섭 <wnstjq7979@gmail.com>
2024-06-02 15:58:25 +0900

    feat: 구글, 카카오, 애플 로그인 유저 정보 조회 로직 한 메소드로 통합

--- src/main/kotlin/com/soongan/soonganbackend/persistence/member/MemberService.kt
+++ src/main/kotlin/com/soongan/soonganbackend/persistence/member/MemberService.kt
@@ -15,17 +15,12 @@ class MemberService(
         val provider = loginDto.provider
         val accessToken = loginDto.accessToken
 
-        val userInfo = when (provider) {
-            "google" -> getGoogleUserInfo(accessToken)
+        val url = when (provider) {
+            "google" -> "https://www.googleapis.com/oauth2/v2/userinfo"
+            "kakao" -> "https://kapi.kakao.com/v2/user/me"
+            "apple" -> "https://appleid.apple.com/auth/user"
             else -> throw IllegalArgumentException("지원하지 않는 로그인 제공자입니다: $provider")
         }
-        println(userInfo)
-
-        return "provider: $provider, accessToken: $accessToken"
-    }
-
-    fun getGoogleUserInfo(accessToken: String): String {
-        val url = "https://www.googleapis.com/oauth2/v2/userinfo"
 
         val client = OkHttpClient()
         val request = Request.Builder()
@@ -39,11 +34,8 @@ class MemberService(
             response.body?.string(),
             Map::class.java
         )
+        println(userInfo)
 
-        if (userInfo["error"] != null) {
-            throw Exception("구글 사용자 정보를 가져오는데 실패했습니다: ${userInfo["error"]}")
-        }
-
-        return userInfo["email"].toString()
+        return "provider: $provider, accessToken: $accessToken"
     }
 }
