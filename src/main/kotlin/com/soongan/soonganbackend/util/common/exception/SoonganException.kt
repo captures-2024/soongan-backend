40c54517a2da95a9fdda93618159e73f6e0923e5
Jaehyuk Choi <jaehyuk.choi96@linecorp.com>
2024-06-04 23:14:03 +0900

    [captures-2024#8] config: add default value for StatusCode

--- src/main/kotlin/com/soongan/soonganbackend/util/common/exception/SoonganException.kt
+++ src/main/kotlin/com/soongan/soonganbackend/util/common/exception/SoonganException.kt
@@ -2,7 +2,7 @@ package com.soongan.soonganbackend.util.common.exception
 
 class SoonganException(
     val statusCode: StatusCode,
-    val detailMessage: String
+    val detailMessage: String = ""
 ): RuntimeException(statusCode.message + detailMessage) {
 
 }
