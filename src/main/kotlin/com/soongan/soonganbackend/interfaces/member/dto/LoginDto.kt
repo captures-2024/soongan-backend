2a3fa78e5070eb379578c1c6136a59aa02d5a621
Jaehyuk Choi <jaehyuk.choi96@linecorp.com>
2024-06-27 00:33:39 +0900

    [captures-2024#21] config: rename package interface to interface`s`

--- src/main/kotlin/com/soongan/soonganbackend/interfaces/member/dto/LoginDto.kt
+++ src/main/kotlin/com/soongan/soonganbackend/interfaces/member/dto/LoginDto.kt
@@ -1,4 +1,4 @@
-package com.soongan.soonganbackend.`interface`.member.dto
+package com.soongan.soonganbackend.interfaces.member.dto
 
 import com.soongan.soonganbackend.enums.Provider
 import jakarta.validation.constraints.NotNull
