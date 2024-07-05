9f9bb1b20c425c27fbde434b84cf6473b73980cb
송준섭 <wnstjq7979@gmail.com>
2024-07-05 11:22:34 +0900

    [captures2024-#26] config: SwaggerConfig 추가

---
+++ src/main/kotlin/com/soongan/soonganbackend/config/SwaggerConfig.kt
@@ -0,0 +1,37 @@
+package com.soongan.soonganbackend.config
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun openApi(): OpenAPI {
+        val securityRequirement = SecurityRequirement().addList("JWT")
+        val components = Components().addSecuritySchemes("JWT", SecurityScheme()
+            .name("JWT")
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+        )
+
+        return OpenAPI()
+            .components(Components())
+            .info(apiInfo())
+            .addSecurityItem(securityRequirement)
+            .components(components)
+    }
+
+    fun apiInfo(): Info {
+        return Info()
+            .title("Soongan API")
+            .description("Soongan API Documentation")
+            .version("1.0.0")
+    }
+}
