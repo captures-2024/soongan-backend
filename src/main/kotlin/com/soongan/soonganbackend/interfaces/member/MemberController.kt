586665b705473d350687686522f23fb7d1e49059
송준섭 <wnstjq7979@gmail.com>
2024-07-05 17:54:38 +0900

    [captures2024-#26] config: MemberController에 Swagger 설명 어노테이션들 추가

--- src/main/kotlin/com/soongan/soonganbackend/interfaces/member/MemberController.kt
+++ src/main/kotlin/com/soongan/soonganbackend/interfaces/member/MemberController.kt
@@ -4,6 +4,9 @@ import com.soongan.soonganbackend.interfaces.member.dto.LoginRequestDto
 import com.soongan.soonganbackend.interfaces.member.dto.LoginResponseDto
 import com.soongan.soonganbackend.service.member.MemberService
 import com.soongan.soonganbackend.util.common.constant.Uri
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ExampleObject
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -12,10 +15,12 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(Uri.MEMBERS)
+@Tag(name = "Member Apis", description = "회원 관리 API")
 class MemberController(
     private val memberService: MemberService
 ) {
 
+    @Operation(summary = "로그인 Api", description = "idToken을 이용하여 로그인을 수행하고, JWT 토큰을 발급합니다.")
     @PostMapping(Uri.LOGIN)
     fun login(@RequestBody @Valid loginDto: LoginRequestDto): LoginResponseDto {
         return memberService.login(loginDto)
