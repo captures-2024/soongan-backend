86ec1196253254b0a53f1ce2bb3ada529b4e9b6f
송준섭 <wnstjq7979@gmail.com>
2024-07-05 17:55:56 +0900

    [captures2024-#26] config: PostLikeController에 Swagger 설명 어노테이션들 추가

--- src/main/kotlin/com/soongan/soonganbackend/interfaces/postLike/PostLikeController.kt
+++ src/main/kotlin/com/soongan/soonganbackend/interfaces/postLike/PostLikeController.kt
@@ -5,6 +5,8 @@ import com.soongan.soonganbackend.interfaces.postLike.dto.PostLikeResponseDto
 import com.soongan.soonganbackend.service.postLike.PostLikeService
 import com.soongan.soonganbackend.util.common.constant.Uri
 import com.soongan.soonganbackend.util.common.dto.MemberDetail
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -13,11 +15,13 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(Uri.LIKE)
+@Tag(name = "Post Like Apis", description = "게시글 좋아요 관련 API")
 class PostLikeController (
     private val postLikeService: PostLikeService
 ){
 
     @PutMapping
+    @Operation(summary = "게시글 좋아요 Api", description = "게시글에 좋아요를 추가합니다.")
     fun addLikePost(
         @RequestBody postLikeRequest: PostLikeRequestDto,
         @AuthenticationPrincipal loginMember: MemberDetail
