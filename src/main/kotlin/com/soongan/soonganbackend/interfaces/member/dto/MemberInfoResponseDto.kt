ba2e5f55506290ca13bd8ff97846e6a095c6016a
송준섭 <wnstjq7979@gmail.com>
2024-08-06 14:23:57 +0900

    [captures2024-#49] fix: MemberInfoResponseDto에 nickname, birthDate, profileImageUrl nullable하게 수정

--- src/main/kotlin/com/soongan/soonganbackend/interfaces/member/dto/MemberInfoResponseDto.kt
+++ src/main/kotlin/com/soongan/soonganbackend/interfaces/member/dto/MemberInfoResponseDto.kt
@@ -5,17 +5,17 @@ import java.time.LocalDate
 
 data class MemberInfoResponseDto(
     val email: String,
-    val nickname: String,
-    val birthDate: LocalDate,
-    val profileImageUrl: String
+    val nickname: String?,
+    val birthDate: LocalDate?,
+    val profileImageUrl: String?
 ) {
     companion object {
         fun from(member: MemberEntity): MemberInfoResponseDto {
             return MemberInfoResponseDto(
                 email = member.email,
-                nickname = member.nickname!!,
-                birthDate = member.birthDate!!,
-                profileImageUrl = member.profileImageUrl!!
+                nickname = member.nickname,
+                birthDate = member.birthDate,
+                profileImageUrl = member.profileImageUrl
             )
         }
     }
