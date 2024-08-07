3de1868a48e66b463cf48f16ee3ff19b0865017e
송준섭 <wnstjq7979@gmail.com>
2024-08-07 10:47:11 +0900

    [captures2024-#48] config: fcm_token 테이블 sql 정보 추가

---
+++ src/main/resources/sql/ddl/008-fcm-token.sql
@@ -0,0 +1,12 @@
+CREATE TABLE fcm_token
+(
+    id              bigint primary key auto_increment,
+    member_id       bigint          not null,
+    token           varchar(255)    not null,
+    device_type     varchar(20)     not null,
+    created_at      datetime        null,
+    updated_at      datetime        null
+);
+
+create index fcm_token_idx_member_id on fcm_token (member_id);
+create index fcm_token_idx_device_type on fcm_token (device_type);
