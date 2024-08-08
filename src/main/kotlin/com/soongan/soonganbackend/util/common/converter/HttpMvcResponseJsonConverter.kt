c87d69a05d0536af4886f2f1496e72507e2ce21f
Jaehyuk Choi <jaehyuk.choi96@linecorp.com>
2024-08-08 14:39:15 +0900

    [captures2024-captures-2024#53] config: logging for filter error

---
+++ src/main/kotlin/com/soongan/soonganbackend/util/common/converter/HttpMvcResponseJsonConverter.kt
@@ -0,0 +1,15 @@
+package com.soongan.soonganbackend.util.common.converter
+
+import jakarta.servlet.http.HttpServletResponse
+
+object HttpMvcResponseJsonConverter {
+
+    fun <T> writeJsonResponse(response: HttpServletResponse, responseType: T?){
+        ObjectJsonConverter.serialize(responseType)?.let {
+            response.contentType = "application/json"
+            response.characterEncoding = "UTF-8"
+            response.writer.write(it)
+        }
+            ?: response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR)
+    }
+}
--- src/main/resources/sql/ddl/007-weekly_contest_post.sql
+++ src/main/resources/sql/ddl/007-weekly_contest_post.sql
@@ -5,7 +5,9 @@ create table weekly_contest_post
     member_id         bigint       not null,
     image_url         varchar(255) not null,
     ranking           int          not null,
-    like_count        int default 0
+    like_count        int default 0,
+    created_at    datetime     null,
+    updated_at    datetime     null
 );
 
 create index weekly_contest_post_idx_weekly_contest_id_ranking on weekly_contest_post (weekly_contest_id, ranking);
