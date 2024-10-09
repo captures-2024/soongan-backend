29d467628c5f1c0d2e3a61deddc7faec92163fd5
SongJSeop <wnstjq7979@gmail.com>
2024-10-09 19:42:52 +0900

    [captures2024-#69] refactor: build.gradle.kts에 prepareKotlinBuildScriptModel task 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,30 +45,8 @@ subprojects {
     tasks.withType<Test> {
         useJUnitPlatform()
     }
-}
 
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
-    implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.5")
-    implementation("com.google.api-client:google-api-client:2.2.0")
-    implementation("com.google.cloud:spring-cloud-gcp-starter-storage")
-    implementation("com.nimbusds:nimbus-jose-jwt:9.37.3")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
-    implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
-    implementation("com.google.firebase:firebase-admin:9.2.0")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
-    runtimeOnly("com.h2database:h2")
-    runtimeOnly("com.mysql:mysql-connector-j")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.batch:spring-batch-test")
-    testImplementation("org.springframework.security:spring-security-test")
+    tasks.register(
+        "prepareKotlinBuildScriptModel"
+    )
 }
