d141ab705f657fb0ed0d2f84bc29dddc2c45b394
Jaehyuk Choi <jaehyuk.choi96@linecorp.com>
2024-10-12 13:04:55 +0900

    [captures2024-captures-2024#43] add mockK test dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ subprojects {
         implementation("org.springframework.boot:spring-boot-starter")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
         testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+        testImplementation("io.mockk:mockk:1.13.13")
         testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     }
 
