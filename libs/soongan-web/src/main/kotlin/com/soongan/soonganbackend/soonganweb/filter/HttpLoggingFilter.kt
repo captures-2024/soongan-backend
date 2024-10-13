aa7dd93f35d324e925fd13dc92c6e2171f393aca
SongJSeop <wnstjq7979@gmail.com>
2024-10-13 21:35:12 +0900

    [captures2024-captures-2024#75] feat: HttpLoggingFilter 추가

---
+++ libs/soongan-web/src/main/kotlin/com/soongan/soonganbackend/soonganweb/filter/HttpLoggingFilter.kt
@@ -0,0 +1,32 @@
+package com.soongan.soonganbackend.soonganweb.filter
+
+import com.soongan.soonganbackend.soongansupport.util.constant.ColorCode
+import com.soongan.soonganbackend.soongansupport.util.constant.MdcConstant
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
+import org.slf4j.MDC
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE + 1)
+class HttpLoggingFilter: OncePerRequestFilter() {
+
+    private val httpLogger = LoggerFactory.getLogger(HttpLoggingFilter::class.java)
+
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+        val requestTime = MDC.get(MdcConstant.REQUEST_TIME)?.toLong() ?: System.currentTimeMillis()
+        val requestUuid = MDC.get(MdcConstant.UUID) ?: "N/A"
+
+        httpLogger.info("${ColorCode.GREEN}[${MDC.get(MdcConstant.UUID)}]${ColorCode.CYAN}[Request]${ColorCode.RESET} ${request.method} ${request.requestURI}")
+
+        filterChain.doFilter(request, response)
+
+        val spendTime = System.currentTimeMillis() - requestTime
+        httpLogger.info("${ColorCode.GREEN}[${requestUuid}]${ColorCode.BLUE}[Response]${ColorCode.RESET} status: ${response.status} ${ColorCode.YELLOW}- ${spendTime}ms${ColorCode.RESET}")
+    }
+}
