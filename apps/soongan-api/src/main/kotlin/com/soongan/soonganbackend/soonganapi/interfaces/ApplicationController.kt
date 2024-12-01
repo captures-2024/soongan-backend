9858a9d113de0a39f4f9f58cc428d0f84ea4081c
SongJSeop <wnstjq7979@gmail.com>
2024-12-01 15:32:25 +0900

    [captures2024-captures-2024#93] refactor: health 체크 api 스웨거에서 무시하도록 수정

--- apps/soongan-api/src/main/kotlin/com/soongan/soonganbackend/soonganapi/interfaces/ApplicationController.kt
+++ apps/soongan-api/src/main/kotlin/com/soongan/soonganbackend/soonganapi/interfaces/ApplicationController.kt
@@ -1,9 +1,11 @@
 package com.soongan.soonganbackend.soonganapi.interfaces
 
+import io.swagger.v3.oas.annotations.Hidden
 import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Hidden
 @RestController
 class ApplicationController(
     private val env: Environment
