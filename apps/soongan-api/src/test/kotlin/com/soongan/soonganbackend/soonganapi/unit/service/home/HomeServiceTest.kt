5843216d2e3610e532053efd949ba509966df8c1
SongJSeop <wnstjq7979@gmail.com>
2024-12-29 21:26:27 +0900

    [captures2024-captures-2024#113] test: HomeServiceTest 추가

---
+++ apps/soongan-api/src/test/kotlin/com/soongan/soonganbackend/soonganapi/unit/service/home/HomeServiceTest.kt
@@ -0,0 +1,70 @@
+package com.soongan.soonganbackend.soonganapi.unit.service.home
+
+import com.soongan.soonganbackend.soonganapi.service.home.HomeService
+import com.soongan.soonganbackend.soonganapi.service.weeklyContest.validator.WeeklyContestValidator
+import com.soongan.soonganbackend.soonganpersistence.storage.member.MemberEntity
+import com.soongan.soonganbackend.soonganpersistence.storage.weeklyContest.WeeklyContestEntity
+import com.soongan.soonganbackend.soonganpersistence.storage.weeklyContestPost.WeeklyContestPostAdapter
+import com.soongan.soonganbackend.soonganpersistence.storage.weeklyContestPost.WeeklyContestPostEntity
+import com.soongan.soonganbackend.soongansupport.domain.ProviderEnum
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+
+class HomeServiceTest {
+    private lateinit var weeklyContestPostAdapter: WeeklyContestPostAdapter
+    private lateinit var weeklyContestValidator: WeeklyContestValidator
+    private lateinit var homeService: HomeService
+
+    @BeforeEach
+    fun setUp() {
+        weeklyContestPostAdapter = mockk()
+        weeklyContestValidator = mockk()
+        homeService = HomeService(weeklyContestPostAdapter, weeklyContestValidator)
+    }
+
+    @Test
+    fun `홈 화면 정보 조회 성공`() {
+        // given
+        val loginMember = MemberEntity(
+            id = 1,
+            email = "test@example.com",
+            provider = ProviderEnum.GOOGLE
+        )
+        val weeklyContest = WeeklyContestEntity(
+            id = 1,
+            round = 1,
+            subject = "test-subject",
+            startAt = LocalDateTime.now().minusDays(1),
+            endAt = LocalDateTime.now().plusDays(1),
+        )
+        val homeWeeklyContestPostList = listOf(
+            WeeklyContestPostEntity(
+                id = 1,
+                member = loginMember,
+                weeklyContest = weeklyContest,
+                imageUrl = "test-image-url",
+            ),
+            WeeklyContestPostEntity(
+                id = 2,
+                member = loginMember,
+                weeklyContest = weeklyContest,
+            )
+        )
+
+        // mock
+        every { weeklyContestValidator.getWeeklyContestIfValidRound() } returns weeklyContest
+        every { weeklyContestPostAdapter.getAllWeeklyContestPostByMemberAndWeeklyContest(loginMember, weeklyContest) } returns homeWeeklyContestPostList
+
+        // when
+        val homeResponseDto = homeService.getHome(loginMember)
+
+        // then
+        assert(homeResponseDto.contestInfo.subject == weeklyContest.subject)
+        assert(homeResponseDto.contestInfo.startAt == weeklyContest.startAt)
+        assert(homeResponseDto.contestInfo.endAt == weeklyContest.endAt)
+        assert(homeResponseDto.postInfo.size == homeWeeklyContestPostList.size)
+    }
+}
