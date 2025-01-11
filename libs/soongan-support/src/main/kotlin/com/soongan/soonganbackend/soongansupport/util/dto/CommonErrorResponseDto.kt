d8a936024eb7ab1d215b8e98ff65886fb55f887d
Jaehyuk Choi <jaehyuk.choi96@linecorp.com>
2025-01-11 16:30:26 +0900

    fix Common Error Response

--- libs/soongan-support/src/main/kotlin/com/soongan/soonganbackend/soongansupport/util/dto/CommonErrorResponseDto.kt
+++ libs/soongan-support/src/main/kotlin/com/soongan/soonganbackend/soongansupport/util/dto/CommonErrorResponseDto.kt
@@ -5,15 +5,18 @@ import com.soongan.soonganbackend.soongansupport.util.exception.StatusCode
 open class CommonErrorResponseDto(
     open val statusCode: Int,
     open val message: String,
+    open val detailMessage: String? = null
 ) {
     companion object {
         fun from(
             statusCode: StatusCode,
-            errorMessage: String? = null
+            errorMessage: String? = null,
+            detailMessage: String? = null
         ): CommonErrorResponseDto =
             CommonErrorResponseDto(
                 statusCode = statusCode.code,
-                message = errorMessage ?: statusCode.message
+                message = errorMessage ?: statusCode.message,
+                detailMessage = detailMessage
             )
     }
 }
