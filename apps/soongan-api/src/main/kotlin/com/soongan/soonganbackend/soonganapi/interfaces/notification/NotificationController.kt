9c03015c88389227eaec9202216bd33130f6db59
SongJSeop <wnstjq7979@gmail.com>
2025-03-29 22:41:57 +0900

    fix: 알림 읽음 처리, 삭제 Api의 경로 수정

--- apps/soongan-api/src/main/kotlin/com/soongan/soonganbackend/soonganapi/interfaces/notification/NotificationController.kt
+++ apps/soongan-api/src/main/kotlin/com/soongan/soonganbackend/soonganapi/interfaces/notification/NotificationController.kt
@@ -12,8 +12,8 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -45,25 +45,24 @@ class NotificationController(
         return notificationService.getNotifications(loginMember, type)
     }
 
-    @PostMapping
+    @PostMapping("/{notificationId}/read")
     @Operation(
         summary = "알림 읽음 처리 Api",
         description = "알림을 읽음 처리합니다.",
         security = [SecurityRequirement(name = "JWT")]
     )
-    fun readNotification(@RequestBody notificationId: Long): Long {
-        return notificationService.readNotification(notificationId)
+    fun readNotification(@LoginMember loginMember: MemberEntity, @PathVariable("notificationId") notificationId: Long): Long {
+        return notificationService.readNotification(loginMember, notificationId)
     }
 
     // TODO: 소명 완료 시 삭제?
-    @DeleteMapping
+    @DeleteMapping("/{notificationId}")
     @Operation(
         summary = "알림 삭제 Api",
         description = "알림을 삭제합니다.",
         security = [SecurityRequirement(name = "JWT")]
     )
-    fun deleteNotification(@RequestBody notificationId: Long) {
-        notificationService.deleteNotification(notificationId)
+    fun deleteNotification(@LoginMember loginMember: MemberEntity, @PathVariable("notificationId") notificationId: Long): Unit {
+        notificationService.deleteNotification(loginMember, notificationId)
     }
-
 }
