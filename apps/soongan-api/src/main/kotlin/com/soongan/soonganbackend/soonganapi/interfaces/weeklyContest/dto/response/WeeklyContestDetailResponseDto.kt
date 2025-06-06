7e757cbeebe5b9a001f40f5505c025eecf218705
SongJSeop <wnstjq7979@gmail.com>
2025-06-06 14:53:14 +0900

    refactor: 역대 주간 콘테스트 상세 조회 api 응답에 1등 사진은 분리

--- apps/soongan-api/src/main/kotlin/com/soongan/soonganbackend/soonganapi/interfaces/weeklyContest/dto/response/WeeklyContestDetailResponseDto.kt
+++ apps/soongan-api/src/main/kotlin/com/soongan/soonganbackend/soonganapi/interfaces/weeklyContest/dto/response/WeeklyContestDetailResponseDto.kt
@@ -6,22 +6,44 @@ import io.swagger.v3.oas.annotations.media.Schema
 @Schema(description = "역대 주간 콘테스트 상세 조회 응답 DTO")
 data class WeeklyContestDetailResponseDto(
     val postsCount: Int,
-    val top7Posts: List<TopPostResponseDto>
+    val firstPrizePost: FirstPrizePostResponseDto,
+    val otherTop7Posts: List<TopPostResponseDto>
 ) {
+    @Schema(description = "역대 주간 콘테스트 1위 게시글 응답 DTO")
+    data class FirstPrizePostResponseDto(
+        val postId: Long,
+        val title: String,
+        val imageUrl: String,
+        val nickname: String?,
+        val score: Int,
+    ) {
+        companion object {
+            fun from(weeklyContestFinalEntity: WeeklyContestFinalEntity): FirstPrizePostResponseDto {
+                return FirstPrizePostResponseDto(
+                    postId = weeklyContestFinalEntity.weeklyContestPost.id!!,
+                    title = weeklyContestFinalEntity.weeklyContestPost.title,
+                    imageUrl = weeklyContestFinalEntity.weeklyContestPost.imageUrl,
+                    nickname = weeklyContestFinalEntity.weeklyContestPost.member.nickname,
+                    score = weeklyContestFinalEntity.score
+                )
+            }
+        }
+    }
+
     @Schema(description = "역대 주간 콘테스트 상위 게시글 응답 DTO")
     data class TopPostResponseDto(
-        val id: Long,
         val postId: Long,
+        val imageUrl: String,
         val nickname: String?,
-        val ranking: Int?,
+        val ranking: Int,
         val score: Int,
     ) {
 
         companion object {
             fun from(weeklyContestFinalEntity: WeeklyContestFinalEntity): TopPostResponseDto {
                 return TopPostResponseDto(
-                    id = weeklyContestFinalEntity.id!!,
                     postId = weeklyContestFinalEntity.weeklyContestPost.id!!,
+                    imageUrl = weeklyContestFinalEntity.weeklyContestPost.imageUrl,
                     nickname = weeklyContestFinalEntity.weeklyContestPost.member.nickname,
                     ranking = weeklyContestFinalEntity.ranking,
                     score = weeklyContestFinalEntity.score
@@ -32,9 +54,16 @@ data class WeeklyContestDetailResponseDto(
 
     companion object {
         fun from(postsCount: Int, top7Posts: List<WeeklyContestFinalEntity>): WeeklyContestDetailResponseDto {
+            if (top7Posts.isEmpty()) {
+                throw IllegalArgumentException("Top posts list cannot be empty")
+            }
+            val sortedTop7Posts = top7Posts.sortedBy { it.ranking }
+            val firstPrizePost = FirstPrizePostResponseDto.from(sortedTop7Posts.first())
+            val otherTop7Posts = sortedTop7Posts.drop(1).map { TopPostResponseDto.from(it) }
             return WeeklyContestDetailResponseDto(
                 postsCount = postsCount,
-                top7Posts = top7Posts.map { TopPostResponseDto.from(it) }
+                firstPrizePost = firstPrizePost,
+                otherTop7Posts = otherTop7Posts
             )
         }
     }
