eef4b34ad027adeb9c4c7594fc02a7263462542c
SongJSeop <wnstjq7979@gmail.com>
2025-06-23 00:51:09 +0900

    fix: WeeklyContestPostEntity에서 deletedReason에 @Enumberate 추가

--- libs/soongan-persistence/src/main/kotlin/com/soongan/soonganbackend/soonganpersistence/storage/weeklyContestPost/WeeklyContestPostEntity.kt
+++ libs/soongan-persistence/src/main/kotlin/com/soongan/soonganbackend/soonganpersistence/storage/weeklyContestPost/WeeklyContestPostEntity.kt
@@ -6,6 +6,8 @@ import com.soongan.soonganbackend.soongansupport.domain.DeletedReasonEnum
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EntityListeners
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -60,6 +62,7 @@ data class WeeklyContestPostEntity(
     val deletedAt: LocalDateTime? = null,
 
     @Column(name = "deleted_reason")
+    @Enumerated(EnumType.STRING)
     val deletedReason: DeletedReasonEnum? = null,
 ) {
 
