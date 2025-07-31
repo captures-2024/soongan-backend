8daecb149657526df8d55ea2a08b32b3b4dc92e7
SongJSeop <wnstjq7979@gmail.com>
2025-07-31 21:08:41 +0900

    fix: 소명 엔티티 수정

--- libs/soongan-persistence/src/main/resources/sql/ddl/013-explain.sql
+++ libs/soongan-persistence/src/main/resources/sql/ddl/013-explain.sql
@@ -1,9 +1,9 @@
-CREATE TABLE explain
+CREATE TABLE explains
 (
     id          bigint primary key auto_increment,
     target_id   bigint      not null,
     target_type varchar(20) not null,
-    explain     text        not null,
+    explains     text        not null,
     created_at  datetime null,
     updated_at  datetime null
 );
